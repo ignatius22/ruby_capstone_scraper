5b59413e25e0d1a1036aaedea5a70a1791db0c07
Ignatius22 <igee54077@gmail.com>
2020-07-11 11:36:12 +0100

    add parse_url method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,6 +2,23 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'pry'
+
+
+
+
 class Scraper
- #we will be adding code here shortly
+attr_reader :url, :position , :company
+	def initialize(position, company)
+		@position = position.capitalize
+		@company = company.capitalize
+		@url = 'https://remotive.io/remote-jobs'
+	end
+
+	def parse_url(url)
+		unparse_url = HTTParty.get(url)
+		Nokogiri::HTML(unparse_url)
+	end
+
+	
+
 end
