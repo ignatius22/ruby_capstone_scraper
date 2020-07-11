60d7dca5e95e84800470f0d2edf176133d8d6e1a
Ignatius22 <igee54077@gmail.com>
2020-07-11 18:09:42 +0100

    add display method in controller

---
+++ bin/controller.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/scraper.rb'
+require_relative '../lib/page.rb'
+
+scrapper_obj = Scrapper.new('https://remotive.io/')
+scraped = scrapper_obj.scrape
+page = Page.new(scraped)
+arr = page.details
+
+# rubocop disable: Layout/LineLength
+def display_details(details)
+  count = 1
+  details.each do |val|
+    puts '__________________________________________________________________________________________________________'
+ 
+		puts "#{count})details
+
+					position:
+					#{val[:position]} 
+					company: #{val[:company]}
+					location: #{val[:location]}
+					"
+    count += 1
+  end
+end
+# rubocop enable: Layout/LineLength
+
+display_details(arr)
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,19 +6,47 @@ require 'pry'
 
 
 
-class Scraper
-attr_reader :url, :position , :company
-	def initialize(position, company)
-		@position = position.capitalize
-		@company = company.capitalize
-		@url = 'https://remotive.io/remote-jobs'
-	end
+# class Scraper
+# attr_reader :url, :position , :company
+# 	def initialize(position, company)
+# 		@position = position.capitalize
+# 		@company = company.capitalize
+# 		@url = 'https://remotive.io'
+# 	end
 
-	def parse_url(url)
-		unparse_url = HTTParty.get(url)
-		Nokogiri::HTML(unparse_url)
-	end
+# 	def parse_url(url)
+# 		unparse_url = HTTParty.get(url)
+# 		Nokogiri::HTML(unparse_url)
+# 	end
 
-	
+# 	def scrape
+# 		parse_page = parse_url(@url)
+# 		byebug
+# 	end
+# end
 
+
+
+class Scrapper
+  attr_reader :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def scrape
+    scraped_pages = []
+    page_body = HTTParty.get(url).body
+    doc = Nokogiri::HTML(page_body)
+    doc.css('div.job-details').each do |node|
+			scraped_pages << node
+    end
+    scraped_pages
+  end
 end
+
+
+# job = Scrapper.new("ruby developer")
+
+# job.scrape
+
