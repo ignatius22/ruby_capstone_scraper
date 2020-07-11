9f47494ca9e58c1a08a6979f8c0c32b75b60c4d2
Ignatius22 <igee54077@gmail.com>
2020-07-11 18:22:38 +0100

    add change to page.rb

--- lib/page.rb
+++ lib/page.rb
@@ -1,6 +1,5 @@
 class Page
   attr_reader :pages
-
   def initialize(pages)
     @pages = pages
   end
@@ -8,10 +7,11 @@ class Page
   def details
     details_arr = []
     pages.each do |node|
-      details_hash = { position: node.css('div.position').text,
-                       company: node.css('div.company').text,
-											 location: node.css('span.location').text
-		}
+			details_hash = { 
+				 position: node.css('div.position').text,
+        company: node.css('div.company').text,
+				location: node.css('span.location').text
+				}
       details_arr << details_hash
     end
     details_arr
